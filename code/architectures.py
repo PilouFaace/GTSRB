0505deef143bb7a8b94356eb7d377133919a800f
PilouFaace <pierrelouisvanacker@gmail.com>
2018-06-06 22:21:38 +0200

    fix ../data

--- code/architectures.py
+++ code/architectures.py
@@ -109,3 +109,47 @@ class VGG(nn.Module):
         x = x.view(x.size(0), -1)  # Flatten
         x = self.classifier(x)
         return x
+
+
+class VGG_bn(nn.Module):
+    def __init__(self):
+        super(VGG_bn, self).__init__()
+        # Training hyperparameters
+        self.lr = 1e-4
+        self.epochs = 40
+        self.batch_size = 32
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(128 * 4 * 4, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 10),
+            nn.Softmax(dim=1)
+        )
+        # Optimizer and loss function
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)  # Flatten
+        x = self.classifier(x)
+        return x
