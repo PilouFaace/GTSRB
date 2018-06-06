4de6f3171be1e6babae242b37a1d7a264d42b4dc
PilouFaace <pierrelouisvanacker@gmail.com>
2018-06-06 16:11:41 +0200

    fix ../data

--- code/gtsrb_loader.py
+++ code/gtsrb_loader.py
@@ -94,14 +94,15 @@ def traite_label(chemin_image):
 # Enregistrement des tenseurs
 
 def save_train(images, labels, couleur):
-    if not os.path.exists('data/' + couleur +):
-        os.makedirs('data/' + couleur )
-    torch.save((images, labels), 'data/' + couleur + '_dist/train.pt')
+    if not os.path.exists('../data/' + couleur):
+        os.makedirs('../data/' + couleur)
+    torch.save((images, labels), '../data/' + couleur + '/train.pt')
+
 
 def save_test(images, labels, couleur):
-    if not os.path.exists('data/' + couleur + '_dist'):
-        os.makedirs('data/' + couleur + '_dist')
-    torch.save((images, labels), 'data/' + couleur + '_dist/test.pt')
+    if not os.path.exists('../data/' + couleur):
+        os.makedirs('../data/' + couleur )
+    torch.save((images, labels), '../data/' + couleur + '/test.pt')
 
 
 # Chargement des tenseurs 
